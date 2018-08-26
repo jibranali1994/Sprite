c3a4b7a5a132971452ec912cbe9e4ed45341be3f
JibranAli <jibran.ali.005@gmail.com>
2018-08-26 19:15:42 +0500

    FightLogic

--- sprite sheet/sprite sheet/Assets/cameramove.cs
+++ sprite sheet/sprite sheet/Assets/cameramove.cs
@@ -11,6 +11,7 @@ public class cameramove : MonoBehaviour {
 
     void Update()
     {
+        if (player)
         transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
     }
 	
---
+++ sprite sheet/sprite sheet/Assets/enemy_move.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+public class enemy_move : MonoBehaviour {
+
+    // Use this for initialization
+
+    Animator anim;
+    void Start () {
+        anim = this.GetComponent<Animator>();
+	}
+	
+    public void Die()
+    {
+        this.GetComponent<Rigidbody2D>().isKinematic = true;
+        anim.SetInteger("transition", -1);
+        Observable.Timer(TimeSpan.FromSeconds(0.5)).Subscribe(x => {
+
+            Destroy(this.gameObject);
+        });
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
